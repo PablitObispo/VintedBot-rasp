aa1d37102f0432a7b0398ee7b2e370bc76ba63d9
PablitObispo <63467719+cramentocode@users.noreply.github.com>
2022-02-13 14:16:22 +0100

    Add files via upload

--- scrapper.py
+++ scrapper.py
@@ -37,11 +37,11 @@ class NotifierVinted:
         options.add_argument("--proxy-server='direct://'")
         options.add_argument("--proxy-bypass-list=*")
         options.add_argument("--start-maximized")
-        options.add_argument('--disable-gpu')
+        options.add_argument("--disable-gpu")
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--no-sandbox')
 
-        service = Service(executable_path="./chromedriver")
+        service = Service(executable_path="/usr/bin/chromedriver")
         self.driver = webdriver.Chrome(service=service, options=options)
         self.driver.get(self.url)
 
